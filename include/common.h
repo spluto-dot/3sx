43a61ddee1aeb7e9d234b2292c7cb3edd2f05ddd
Artem Pstygo <apstygo.dev@gmail.com>
2025-01-15 22:54:38 +0300

    Allow including simple functions from asm in GCC compiled files

--- include/common.h
+++ include/common.h
@@ -5,7 +5,23 @@
 
 #define NULL 0
 
+// Compilation with GNU C Compilier means we are compiling CRI
+#ifdef __GNUC__
+#define INCLUDE_ASM(FOLDER, NAME)                                                                                      \
+    __asm__(".pushsection .text\n"                                                                                     \
+            "\t.align\t3\n"                                                                                            \
+            "\t.globl\t" #NAME "\n"                                                                                    \
+            "\t.ent\t" #NAME "\n" #NAME ":\n"                                                                          \
+            ".include \"" FOLDER "/" #NAME ".s\"\n"                                                                    \
+            "\t.set reorder\n"                                                                                         \
+            "\t.set at\n"                                                                                              \
+            "\t.end\t" #NAME "\n"                                                                                      \
+            ".popsection")
+
+__asm__(".include \"include/cri_macro.inc\"\n");
+#else
 #define INCLUDE_ASM(FOLDER, NAME)
 #define INCLUDE_RODATA(FOLDER, NAME)
+#endif
 
 #endif
