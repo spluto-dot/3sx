10c7a982321ea2b8fad86eb0bf902869b9401fd1
Artem Pstygo <apstygo.dev@gmail.com>
2025-01-17 16:56:00 +0300

    Include other funcs as asm

--- include/cri/private/libadxe/adx_amp.h
+++ include/cri/private/libadxe/adx_amp.h
@@ -1,4 +1,8 @@
 #ifndef ADX_AMP_H
 #define ADX_AMP_H
 
+typedef struct {
+    char pad[0x30];
+} ADXAMP;
+
 #endif // ADX_AMP_H
--- src/anniversary/cri/libadxe/adx_amp.c
+++ src/anniversary/cri/libadxe/adx_amp.c
@@ -3,16 +3,12 @@
 #include <cri/private/libadxe/adx_crs.h>
 #include <memory.h>
 
-typedef struct {
-    char pad[0x300];
-} ADXAMP;
-
-ADXAMP adxamp_obj;
 s32 adxsmp_init_cnt = 0;
+ADXAMP adxamp_obj[16] = { 0 };
 
 void ADXAMP_Init() {
     if (adxsmp_init_cnt == 0) {
-        memset(&adxamp_obj, 0, sizeof(ADXAMP));
+        memset(&adxamp_obj, 0, sizeof(adxamp_obj));
     }
 
     adxsmp_init_cnt += 1;
@@ -20,6 +16,36 @@ void ADXAMP_Init() {
 
 void ADXAMP_Finish() {
     if (--adxsmp_init_cnt == 0) {
-        memset(&adxamp_obj, 0, sizeof(ADXAMP));
+        memset(&adxamp_obj, 0, sizeof(adxamp_obj));
     }
 }
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_amp", ADXAMP_Create);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_amp", ADXAMP_Destroy);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_amp", ADXAMP_GetStat);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_amp", ADXAMP_Start);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_amp", ADXAMP_Stop);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_amp", adxamp_extract);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_amp", ADXAMP_ExecHndl);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_amp", ADXAMP_ExecServer);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_amp", ADXAMP_GetExtractNumSmpl);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_amp", ADXAMP_SetSfreq);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_amp", ADXAMP_GetSfreq);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_amp", ADXAMP_SetFrmLen);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_amp", ADXAMP_GetFrmLen);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_amp", ADXAMP_SetFrmPrd);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_amp", ADXAMP_GetFrmPrd);
