a4942e6b6ef844420f591d244f323a698b5289b4
Artem Pstygo <apstygo.dev@gmail.com>
2025-06-02 03:39:15 +0300

    Improve the output of not_implemented (#119)

--- src/anniversary/stubs/utils.c
+++ src/anniversary/stubs/utils.c
@@ -1,9 +1,18 @@
 #include "common.h"
 
+#include <execinfo.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BACKTRACE_MAX 100
+
 void not_implemented(const s8 *func) __dead2 {
     fprintf(stderr, "Function not implemented: %s\n", func);
+
+    void *buffer[BACKTRACE_MAX];
+    int nptrs = backtrace(buffer, BACKTRACE_MAX);
+    fprintf(stderr, "Stack trace:\n");
+    backtrace_symbols_fd(buffer, nptrs, fileno(stderr));
+
     abort();
 }
