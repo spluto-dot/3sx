718b8659e5aa96f84d9a03ce93c080db5efb8ebc
foolycooly95 <jackwoodman95@gmail.com>
2025-06-05 15:00:24 +0100

    Some Saver.c functions (#118)
    * Some Saver.c functions
    
    * Update Saver.c with pull request suggestions
    
    * Missed the include for sc_sub.h

--- src/anniversary/sf33rd/Source/Game/Saver.c
+++ src/anniversary/sf33rd/Source/Game/Saver.c
@@ -1,4 +1,7 @@
 #include "sf33rd/Source/Game/Saver.h"
+#include "sf33rd/Source/Game/sc_sub.h"
+#include "sf33rd/Source/Game/workuser.h"
+#include "sf33rd/Source/Game/WORK_SYS.h"
 #include "common.h"
 
 #if defined(TARGET_PS2)
@@ -9,10 +12,67 @@ void Saver_Task(struct _TASK *task_ptr) {
 }
 #endif
 
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/Saver", Saver_Init);
+void Saver_Init(struct _TASK* task_ptr) {
+    task_ptr->r_no[0] = 1;
+    task_ptr->r_no[1] = 0;
+    task_ptr->r_no[2] = 0;
+    task_ptr->r_no[3] = 0;
+    task_ptr->timer = 0;
+}
 
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/Saver", Saver_Check);
+void Saver_Check(struct _TASK *task_ptr) {
+    if (Demo_Flag == 0) {
+        task_ptr->timer = 0;
+        return;
+    }
 
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/Saver", Saver_Move);
+    if ((PLsw[0][0] != 0) || (PLsw[1][0] != 0)) {
+        task_ptr->timer = 0;
+        return;
+    }
 
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/Saver", Saver_Exit);
+    if ((task_ptr->timer += 1) > 18000) {
+        task_ptr->r_no[0]++;
+    }
+}
+
+void Saver_Move(struct _TASK* task_ptr) {
+    if ((PLsw[0][0] != 0) || PLsw[1][0] != 0) {
+        Saver_Init(task_ptr);
+
+    } else {
+        switch (task_ptr->r_no[1]) {
+        case 0:
+            task_ptr->r_no[1] += 1;
+            task_ptr->free[0] = 0;
+            FadeInit();
+            /* fallthrough */
+
+        case 1:
+            FadeOut(1, 4, 0);
+
+            if ((task_ptr->free[0]++) > 0x30) {
+                task_ptr->r_no[1] += 1;
+            }
+            break;
+
+        case 2:
+            ToneDown(0xC8, 0);
+            break;
+        }
+    }
+}
+
+void Saver_Exit(struct _TASK* task_ptr) {
+    switch (task_ptr->r_no[1]) {
+    case 0:
+        task_ptr->r_no[1] += 1;
+        FadeInit();
+        /* fallthrough */
+
+    case 1:
+        if (FadeIn(1, 0xFF, 8) != 0) {
+            Saver_Init(task_ptr);
+        }
+    }
+}
