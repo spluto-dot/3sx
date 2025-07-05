7620670135ccccf5bea0f3bc808d6f0855b59337
foolycooly95 <jackwoodman95@gmail.com>
2025-07-05 13:49:35 +0100

    MMTMCNT (#168)

--- include/sf33rd/Source/Game/MMTMCNT.h
+++ include/sf33rd/Source/Game/MMTMCNT.h
@@ -3,7 +3,9 @@
 
 #include "types.h"
 
+void Purge_com_player_from_mm();
 void Purge_mmtm_area(s16 ix);
+void Purge_memory_of_list(s16 ix);
 void Purge_texcash_of_list(s16 ix);
 void Make_texcash_of_list(s16 ix);
 
--- src/anniversary/sf33rd/Source/Game/MMTMCNT.c
+++ src/anniversary/sf33rd/Source/Game/MMTMCNT.c
@@ -1,40 +1,97 @@
 #include "sf33rd/Source/Game/MMTMCNT.h"
 #include "common.h"
+#include "sf33rd/Source/Game/PLCNT.h"
+#include "sf33rd/Source/Game/RAMCNT.h"
+#include "sf33rd/Source/Game/texcash.h"
+#include "sf33rd/Source/Game/texgroup.h"
 
-INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/MMTMCNT", MM_num);
+const u8 MM_num[32] = { 0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15,
+                        16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 };
 
-INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/MMTMCNT", TM_num);
+const u8 TM_num[24] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23 };
 
-INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/MMTMCNT", pmo_list);
+const u8 pmo_list[9][32] = {
+    { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+    { 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0 },
+    { 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0 },
+    { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0 },
+    { 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0 },
+    { 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0 },
+    { 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0 },
+    { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0 },
+    { 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0 }
+};
 
-INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/MMTMCNT", pto_list);
+const u8 pto_list[9][24] = { { 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 },
+                             { 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 },
+                             { 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0 },
+                             { 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                             { 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0 },
+                             { 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 },
+                             { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 },
+                             { 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 },
+                             { 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 } };
 
-INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/MMTMCNT", mto_list);
+const u8 mto_list[9][24] = { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                             { 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0 },
+                             { 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+                             { 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } };
 
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/MMTMCNT", Purge_com_player_from_mm);
+void Purge_com_player_from_mm() {
+    if (plw[0].wu.operator == 0) {
+        purge_player_texture(0);
+    }
 
-#if defined(TARGET_PS2)
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/MMTMCNT", Purge_mmtm_area);
-#else
+    if (plw[1].wu.operator == 0) {
+        purge_player_texture(1);
+    }
+}
+
 void Purge_mmtm_area(s16 ix) {
-    not_implemented(__func__);
-}
+#if defined(TARGET_PS2)
+    void Purge_memory_of_list(s32 ix);
+    void Purge_texcash_of_list(s32 ix);
 #endif
 
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/MMTMCNT", Purge_memory_of_list);
+    Purge_texcash_of_list(ix);
+    Purge_memory_of_list(ix);
+}
 
-#if defined(TARGET_PS2)
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/MMTMCNT", Purge_texcash_of_list);
-#else
+void Purge_memory_of_list(s16 ix) {
+    s16 i;
+
+    for (i = 0; i < 32; i++) {
+        if (pmo_list[ix][i]) {
+            Purge_memory_of_kind_of_key(MM_num[i]);
+        }
+    }
+}
+
 void Purge_texcash_of_list(s16 ix) {
-    not_implemented(__func__);
-}
+#if defined(TARGET_PS2)
+    void purge_texcash_work(s32 ix);
 #endif
 
-#if defined(TARGET_PS2)
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/MMTMCNT", Make_texcash_of_list);
-#else
-void Make_texcash_of_list(s16 ix) {
-    not_implemented(__func__);
+    s16 i;
+
+    for (i = 0; i < 0x18; i++) {
+        if (pto_list[ix][i]) {
+            purge_texcash_work(i);
+        }
+    }
 }
-#endif
+
+void Make_texcash_of_list(s16 ix) {
+    s16 i;
+
+    for (i = 0; i < 0x18; i++) {
+        if (mto_list[ix][i]) {
+            make_texcash_work(TM_num[i]);
+        }
+    }
+}
