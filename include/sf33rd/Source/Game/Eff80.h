e8106dbda92c7c3332bf9b9c499fa3465bb43306
foolycooly95 <jackwoodman95@gmail.com>
2025-09-20 12:40:39 +0100

    Eff79, Eff80, EFF98 (#221)

---
+++ include/sf33rd/Source/Game/Eff80.h
@@ -0,0 +1,10 @@
+#ifndef EFF80_H
+#define EFF80_H
+
+#include "structs.h"
+#include "types.h"
+
+void effect_80_move(WORK_Other *ewk);
+s32 effect_80_init(WORK_Other *mwk, s16 PL_id, s16 Plate_id, s16 Target_BG);
+
+#endif
--- tools/patch_alignment.py
+++ tools/patch_alignment.py
@@ -15,6 +15,7 @@ special_cases = {
     ("EFF69", ".rodata", 0): 16,
     ("EFF78", ".rodata", 2): 16,
     ("Eff95", ".rodata", 0): 16,
+    ("Eff99", ".rodata", 0): 16,
     ("EFFA6", ".rodata", 0): 16,
     ("EFFF2", ".rodata", 0): 16,
     ("EFFF8", ".rodata", 0): 16,
