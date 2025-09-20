d4a3123fe4d0354b7d7dedf6bad94a7306045e27
foolycooly95 <jackwoodman95@gmail.com>
2025-09-20 12:37:54 +0100

    EFF49, Eff50, Eff51, Eff52 (#217)

--- include/sf33rd/Source/Game/EFF49.h
+++ include/sf33rd/Source/Game/EFF49.h
@@ -1,8 +1,10 @@
 #ifndef EFF49_H
 #define EFF49_H
 
+#include "structs.h"
 #include "types.h"
 
+void effect_49_move(WORK_Other *ewk);
 s32 effect_49_init(s16 vital_new);
 
 #endif
--- include/sf33rd/Source/Game/Eff51.h
+++ include/sf33rd/Source/Game/Eff51.h
@@ -1,8 +1,10 @@
 #ifndef EFF51_H
 #define EFF51_H
 
+#include "structs.h"
 #include "types.h"
 
+void effect_51_move(WORK_Other_CONN *ewk);
 s32 effect_51_init(s16 letter_type, s16 cursor_index, s16 master_player);
 
 #endif
