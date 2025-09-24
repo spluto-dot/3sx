060b6919777b752b0cb3f351ad17fa93e7d155fd
foolycooly95 <jackwoodman95@gmail.com>
2025-09-24 13:09:46 +0100

    EFF90 (#243)

---
+++ include/sf33rd/Source/Game/EFF90.h
@@ -0,0 +1,9 @@
+#ifndef EFF90_H
+#define EFF90_H
+
+#include "structs.h"
+#include "types.h"
+
+void effect_90_move(WORK_Other_CONN *ewk);
+
+#endif
--- src/anniversary/sf33rd/Source/Game/EFFXX.c
+++ src/anniversary/sf33rd/Source/Game/EFFXX.c
@@ -70,6 +70,7 @@
 #include "sf33rd/Source/Game/EFF85.h"
 #include "sf33rd/Source/Game/EFF86.h"
 #include "sf33rd/Source/Game/EFF87.h"
+#include "sf33rd/Source/Game/EFF90.h"
 #include "sf33rd/Source/Game/EFF92.h"
 #include "sf33rd/Source/Game/EFF96.h"
 #include "sf33rd/Source/Game/EFF98.h"
@@ -278,7 +279,7 @@ const void (*effmovejptbl[229])() = {
     effect_87_move,
     effect_dummy_move,
     effect_dummy_move,
-    NULL, // effect_90_move
+    effect_90_move,
     effect_91_move,
     effect_92_move,
     effect_93_move,
